08863c3f1af63e446d44b6fce99ecc0137f9a29d
kalilask4 <kalilask4@gmail.com>
2022-02-20 02:20:12 +0300

    Add base example

---
+++ Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection(); //ресурс. С ним нужно аккуратно рпботать, аккуратно освобождать, аккуратно использовать
+using var channel = connection.CreateModel();
+
+var queueName = "test-queue";
+channel.QueueDeclare(queueName, exclusive: false, autoDelete: false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume(queueName, true, consumer);
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine(message);
+};
+
+
+
+Console.ReadLine();
+
+    
+channel.Close();    
+connection.Close(); //важно не забывать закрыть подключение
---
+++ Producer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+using var connection = factory.CreateConnection(); //ресурс. С ним нужно аккуратно рпботать, аккуратно освобождать, аккуратно использовать
+using var channel = connection.CreateModel();
+
+var queueName = "test-queue";
+channel.QueueDeclare(queueName, exclusive: false, autoDelete: false);
+
+var message = "test message";
+var body = Encoding.UTF8.GetBytes(message);
+channel.BasicPublish(string.Empty, queueName, null, body);
+    
+channel.Close();    
+connection.Close(); //важно не забывать закрыть подключение
